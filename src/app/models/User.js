067f7beb2e2ba96f345494e82104ed8bb30b08d6
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-05-26 17:03:00 -0300

    sequelize config

---
+++ src/app/models/User.js
@@ -0,0 +1,33 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrpyt from 'bcrypt';
+
+class User extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                name: Sequelize.STRING,
+                email: Sequelize.STRING,
+                password: Sequelize.VIRTUAL,
+                password_hash: Sequelize.STRING,
+            },
+            {
+                sequelize,
+            }
+        );
+
+        this.addHook('beforeSave', async (user) => {
+            if(user.password) {
+                user.password_hash = await bcrpyt.hash(user.password, 8);
+            };
+        });
+
+        return this;
+    };
+
+    checkPassword(password) {
+        return bcrpyt.compare(password, this.password_hash);
+    };
+
+};
+
+export default new User;
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'pele@2011',
+    database: 'tasklist',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+};
