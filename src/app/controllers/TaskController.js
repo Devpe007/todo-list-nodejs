422de3539d8b76af976269ad2650bdff332f9832
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-05-27 21:06:11 -0300

    create tasks destroy

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -43,6 +43,24 @@ class TaskController {
 
         return res.json(task);
     };
+
+    async destroy(req, res) {
+        const { task_id } = req.params;
+
+        const task = await Task.findByPk(task_id);
+
+        if(!task) {
+            return res.status(400).json({ error: 'Tarefa não encontrada' });
+        };
+
+        if(task.user_id !== req.userId) {
+            return res.status(401).json({ error: 'Requisição não autorizada' });
+        };
+
+        await task.destroy();
+
+        return res.send();
+    };
 };
 
 export default new TaskController();
